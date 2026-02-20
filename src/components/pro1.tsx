780f1a977db00eb4e7fe801a916aaf5b0a8781f9
harnishsp001 <harnishpoojary@gmail.com>
2026-02-20 10:58:10 +0530

    update1

--- src/components/pro1.tsx
+++ src/components/pro1.tsx
@@ -1,45 +1,75 @@
 import './new2.css'
 import img1 from '../assets/star.png'
+import img2 from '../assets/email.svg'
+import img3 from '../assets/lock.svg'
 
-function pro1() {
-
-
+function Pro1() {
     return (
         <>
             <div className='main'>
                 <div className='top'>
-                    <div className='t1'><img src={img1}/></div>
-                    <div className='t1'><p><h1>Welcome Back</h1></p></div>
-                    <div className='t1'>Login to your account to book videos</div>
+                    <div className='t1'>
+                        <img src={img1} alt="star" />
+                    </div>
+
+                    <div className='t1'>
+                        <h1>Welcome Back</h1>
+                    </div>
+
+                    <div className='t1'>
+                        Login to your account to book videos
+                    </div>
                 </div>
+
                 <div className='middle'>
                     <div className='m1'>Email</div>
-                    <div className='m2'>
-                        <div className='m21'></div>
-                         <div className='m22'><input type="text"  placeholder='user@example.com'/></div>
-                         
-                    </div>
-                    <div className='m3'>Demo:user@example.</div>
-                    <div className='m4'>Password</div>
-                    <div className='m5'>
-                        <div className='m51'></div>
-                         <div className='m52'><input type="text"  placeholder='****************'/></div>
 
+                    <div className='m2'>
+                        <div className='m22'>
+                            <div className='email'>
+                                <img src={img2} alt="email" />
+                            </div>
+
+                            <div className='user'>
+                                <input type="text" placeholder='user@example.com' />
+                            </div>
+                        </div>
                     </div>
-                    <div className='m6'>Demo:password</div>
+
+                    <div className='m3'>Demo: user@example.com</div>
+
+                    <div className='m4'>Password</div>
+
+                    <div className='m5'>
+                        <div className='m51'>
+                            <img src={img3}  />
+                        </div>
+
+                        <div className='m52'>
+                            <input type="password" placeholder='password' />
+                        </div>
+                    </div>
+
+                    <div className='m6'>Demo: password</div>
                 </div>
+
                 <div className='bottom'>
-                    <div className='b1'><button>Login</button></div>
-                    <div className='b2'>Don't have an account? Sign up</div>
+                    <div className='b1'>
+                        <button>Login</button>
+                    </div>
+
+                    <div className='b2'>
+                        Don't have an account? <span style={{color:"blue",marginLeft:"5px"}}>Sign up</span>
+                    </div>
+
                     <div className='b3'>
-                        
-                        <div className='b31'>Celebrity Yogin  .</div>
-                        <div className='b32'>   Admin Login</div>
+                        <div className='b31'>Celebrity Login</div>
+                        <div className='b32'>Admin Login</div>
                     </div>
                 </div>
             </div>
-
         </>
     )
 }
-export default pro1
+
+export default Pro1
